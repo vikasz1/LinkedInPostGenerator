f019ae2e90021114f28420a0b445100ab8aa4bea
Vikas Maury <mauryvikas12345@gmail.com>
2025-09-01 21:08:30 +0530

    wrong code

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,6 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   const start = Date.now();
